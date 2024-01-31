1613e6c20ac83736bc60fce1e56cf5c300d12152
Pravin Nagraj <pavinagraj143@gmail.com>
2024-01-31 17:36:43 +0530

    Create a new file Demo4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Demo from "./Demo";
 import Demo1 from "./Demo1";
 import Demo2 from "./Demo2";
 import Demo3 from "./Demo3";
+import { Demo4 } from "./Demo4";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Demo1 />
       <Demo2 />
       <Demo3 />
+      <Demo4 />
     </div>
   );
 }
