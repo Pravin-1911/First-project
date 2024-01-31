f9bd16ae196ee8b8260a4ae4354edcc22a546f0b
tonradteam23 <76720210+tonradteam23@users.noreply.github.com>
2024-01-31 14:52:28 +0530

    Create new file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import "./App.css";
 import Demo from "./Demo";
+import Demo1 from "./Demo1";
 
 function App() {
   return (
     <div className="App">
       <Demo />
+      <Demo1 />
     </div>
   );
 }
