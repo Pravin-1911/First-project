a57c48c43177cfd1fecfdd1b4459f94481d71a0a
Pravin Nagraj <pavinagraj143@gmail.com>
2024-02-01 16:05:15 +0530

    Updated Demo1 file

--- src/Demo1.js
+++ src/Demo1.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Demo1() {
-  return <div>Hello World...</div>;
+  return (
+    <>
+      <div>Hello World...</div>;<div>Hello GitHub..</div>
+    </>
+  );
 }
